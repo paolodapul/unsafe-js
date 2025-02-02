603ced6a1c37325471f08bed3e900c4a2f6fafc6
Paolo Dapul <paolodapultech@gmail.com>
2025-02-02 17:04:06 +0800

    better if you define what the inferred type is

--- 02.ts
+++ 02.ts
@@ -1,4 +1,4 @@
-function getPayment(id: string): any {
+function getPayment(id: string) {
   return { amount: 100, currency: "USD" };
 }
 let payment = getPayment("123");
@@ -6,4 +6,13 @@ console.log(payment.someUndefinedProp); // technically invalid yet no compile ti
 
 /**
  * If you remove the `any` annotation, `payment.someUndefinedProp` will then be caught as type error by TypeScript
+ */
+
+/**
+ * Removing `any` will give you the following type:
+ * {
+    amount: number;
+    currency: string;
+    }
+ * Much better if you define what that type is e.g. Payment
  */
