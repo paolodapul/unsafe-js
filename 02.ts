42793277bc4545142434acae918f321537ba842e
Paolo Dapul <paolodapultech@gmail.com>
2025-02-02 17:06:26 +0800

    define the inferred type as a standalone type

--- 02.ts
+++ 02.ts
@@ -1,4 +1,4 @@
-function getPayment(id: string) {
+function getPayment(id: string): any {
   return { amount: 100, currency: "USD" };
 }
 let payment = getPayment("123");
@@ -16,3 +16,15 @@ console.log(payment.someUndefinedProp); // technically invalid yet no compile ti
     }
  * Much better if you define what that type is e.g. Payment
  */
+
+type Payment = {
+  amount: Number;
+  currency: "USD";
+};
+
+function getPayment2(id: string): Payment {
+  return { amount: 100, currency: "USD" };
+}
+let payment2 = getPayment2("123");
+// console.log(payment2.someUndefinedProp); Property 'someUndefinedProp' does not exist on type 'Payment'.ts(2339)
+console.log(payment2.amount);
