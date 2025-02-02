bd07d78ecca57f7b15466aceb563922f7ba36069
Paolo Dapul <paolodapultech@gmail.com>
2025-02-02 17:01:41 +0800

    inference will work if you remove the 'any' annotation

--- 02.ts
+++ 02.ts
@@ -3,3 +3,7 @@ function getPayment(id: string): any {
 }
 let payment = getPayment("123");
 console.log(payment.someUndefinedProp); // technically invalid yet no compile time error
+
+/**
+ * If you remove the `any` annotation, `payment.someUndefinedProp` will then be caught as type error by TypeScript
+ */
